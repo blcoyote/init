974ce2a0194e3334304a5a9e343f819ab2436869
blc <30732216+blcoyote@users.noreply.github.com>
2018-06-17 20:33:56 +0200

    .

--- 1.monitor-on.sh
+++ 1.monitor-on.sh
@@ -3,8 +3,8 @@
 #determine wlan adaptors. if more than one, use the last
 wlan=iw dev | awk '$1=="Interface"{print $2}' #need verification on multi wlan adaptor system.
 echo $wlan > ../wlan.tmp
-ifconfig wlan down
-macchanger -r wlan
-iwconfig wlan mode monitor
-ifconfig wlan up
-iwconfig wlan | grep Mode
+ifconfig $wlan down
+macchanger -r $wlan
+iwconfig $wlan mode monitor
+ifconfig $wlan up
+iwconfig $wlan | grep Mode
