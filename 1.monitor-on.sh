15b5eb34654c0d090262426315220bb895ea4a34
blc <30732216+blcoyote@users.noreply.github.com>
2018-06-17 20:44:35 +0200

    1

--- 1.monitor-on.sh
+++ 1.monitor-on.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #determine wlan adaptors. if more than one, use the last
-echo iw dev | awk '$1=="Interface"{print $2}' > ./wlan.tmp #need verification on multi wlan adaptor system.
-wlan='cat ./wlan.tmp'
+wlan=$(iw dev | awk '$1=="Interface"{print $2}') #need verification on multi wlan adaptor system.
+echo $wlan > ./wlan.tmp
 ifconfig $wlan down
 macchanger -r $wlan
 iwconfig $wlan mode monitor
