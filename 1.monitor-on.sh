8e4859479b62f74e4658522301bf017b17ee387b
blcoyote <30732216+blcoyote@users.noreply.github.com>
2018-06-18 16:19:06 +0200

    1

--- 1.monitor-on.sh
+++ 1.monitor-on.sh
@@ -6,17 +6,16 @@ if [ -n wlan ]
     then
         echo "Wireless interface found: $wlan"
         echo $wlan > ./wlan.tmp
-        IFS=$'\n' read -d '' -r -a adaptors < ./wlan.tmp
-        echo "bringing $adaptors[0] down"
-        ifconfig $adaptors[0] down
+        echo "bringing $wlan down"
+        ifconfig $wlan down
         echo "spoofing mac"
-        macchanger -r $adaptors[0]
+        macchanger -r $wlan
         echo "done. Enabling monitor mode"
-        iwconfig $adaptors[0] mode monitor
-        ifconfig $adaptors[0] up
+        iwconfig $wlan mode monitor
+        ifconfig $wlan up
         echo "======================================"
         echo "Wireless interface put in monitor mode"
-        iwconfig $adaptors[0] | grep Mode
+        iwconfig $wlan | grep Mode
         echo "======================================"
     else
         echo "==================================="
