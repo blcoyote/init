41bce4bf326a3cc196a0fb60daf811e5a3802602
blc <30732216+blcoyote@users.noreply.github.com>
2018-06-18 08:24:19 +0200

    1

--- mitm-pwharvest/1.init.sh
+++ mitm-pwharvest/1.init.sh
@@ -3,4 +3,6 @@ echo 1 > /proc/sys/net/ipv4/ip_forward
 iptables -t nat -A PREROUTING -p tcp --destination-port 80 -j REDIRECT --to-port 8080
 #iptables -t nat -L PREROUTING # uncomment for verification printout!
 iptables -I INPUT 1 -p tcp --dport 8080 -j ACCEPT
+echo "================================================================================="
 echo "Run 2, 3 & 4 in seperate terminal instances. Run cleanup.sh remove iptables rules"
+echo "================================================================================="
