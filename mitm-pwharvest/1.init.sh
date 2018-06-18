a4342797039fb139738cd2704016a42ad99d529e
blcoyote <30732216+blcoyote@users.noreply.github.com>
2018-06-18 13:37:50 +0200

    Update 1.init.sh

--- mitm-pwharvest/1.init.sh
+++ mitm-pwharvest/1.init.sh
@@ -6,13 +6,7 @@ iptables -I INPUT 1 -p tcp --dport 8080 -j ACCEPT
 echo "================================================================================="
 echo "Run 2, 3 & 4 in seperate terminal instances. Run cleanup.sh remove iptables rules"
 echo "================================================================================="
-read -n1 -r -p "Press space to continue..." key
-if [ "$key" = '' ]
-    then
+#read -rsp $'Press any key to continue...\n' -n1 key
     xterm -e ./2*
     #xterm -e ./3*
     #xterm -e ./4*
-    else
-    # Anything else pressed, do whatever else.
-    # echo [$key] not empty
-fi
